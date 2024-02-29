30f0559e44e72b8f910090b55c321437dd7ba18d
Victor Kim <vskimjr@gmail.com>
2024-02-29 17:38:42 -0500

    debugging infinite loop on find

--- bsts/find/find.ts
+++ bsts/find/find.ts
@@ -1,4 +1,4 @@
-import { BNodeNum } from "../common/bst";
+import { BNode, BNodeNum } from "../common/bst";
 
 
 /** findRecursively(val): Search from the invoking node for a node with value val.
@@ -13,7 +13,37 @@ function findRecursively(node: BNodeNum | null, val: number): BNodeNum | null {
  * Returns the node, if found; else null. */
 
 function find(node: BNodeNum | null, val: number): BNodeNum | null {
-  return null;
+
+  if (node === null) return null;
+
+  // let currVal : BNodeNum | null = node;
+
+  let currNode : BNodeNum | null = node;
+  let currVal = node.val;
+
+  while (val !== currVal){
+    if (currNode!.val === val) return currNode
+    if (val < currNode!.val) {
+      currNode = node.left;
+    } else if (val > currNode!.val){
+      currNode = node.right;
+    } else {
+      return null
+    }
+  }
+
+
+  // while (node){
+  //   console.log("node", node)
+  //   if (node!.val === val) return node;
+  //   if (val < node!.val){
+  //     node = node.left;
+  //   } else {
+  //     node = node.right;
+  //   }
+  // }
+
+  // return null;
 }
 
 export { findRecursively, find };
